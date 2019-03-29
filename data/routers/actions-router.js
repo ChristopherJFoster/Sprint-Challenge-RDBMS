4d0337556fd92d787874e6ee8c2127a00a421b45
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-03-29 13:29:12 -0400

    Added an additonal error message to actions-router.js that I hope we can talk about.

--- data/routers/actions-router.js
+++ data/routers/actions-router.js
@@ -17,11 +17,22 @@ router.post('/', async (req, res) => {
       const addedAction = await Actions.addAction(req.body);
       res.status(201).json(addedAction);
     } catch (err) {
-      res.status(500).json({
-        error: `There was an error while adding the action data. ${err}`
-      });
+      // See notes below regarding errno 19...
+      if (err.errno === 19) {
+        res.status(400).json({
+          error: `Please supply a valid project ID.`
+        });
+      } else {
+        res.status(500).json({
+          error: `There was an error while adding the action data. ${err}`
+        });
+      }
     }
   }
 });
 
 module.exports = router;
+
+// ...I want to send a specific message about supplying a valid project ID (without making an additional initial server call just to check if the project_id is valid). However, I think errno 19 is a SQLite contraint error, and could mean other things than a foreign key violation. I would like to know how to parse the SQLite error, which looks like this:
+// { [Error: SQLITE_CONSTRAINT: FOREIGN KEY constraint failed] errno: 19, code: 'SQLITE_CONSTRAINT' }
+// I used errno 19 in this case just to show what I was up to.
